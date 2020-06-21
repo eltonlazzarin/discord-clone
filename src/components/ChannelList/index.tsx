1cdb2da01b2c66dde3508aa2d62f11144b5681bd
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-06-21 12:28:59 -0300

    main channel list

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import ChannelButton from '../ChannelButton';
+
+import { Container, Category, AddCategoryIcon } from './styles';
+
+const ChannelList: React.FC = () => {
+  return (
+    <Container>
+      <Category>
+        <span>Canais de texto</span>
+        <AddCategoryIcon />
+      </Category>
+
+      <ChannelButton channelName="chat-livre" selected />
+      <ChannelButton channelName="lolzinho" />
+      <ChannelButton channelName="csgo" />
+      <ChannelButton channelName="valorant" />
+    </Container>
+  );
+};
+
+export default ChannelList;
