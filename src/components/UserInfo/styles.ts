9aa383bb694ece58da13f8092afd39d063e0be43
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-06-21 12:36:52 -0300

    user info styles

---
+++ src/components/UserInfo/styles.ts
@@ -0,0 +1,119 @@
+import styled from 'styled-components';
+import { Mic, Headset, Settings } from 'styled-icons/material';
+
+export const Container = styled.div`
+  grid-area: UI;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 10px;
+  background-color: var(--quaternary);
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 0px 0px;
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Avatar = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+
+  background: url('https://avatars1.githubusercontent.com/u/53025782?s=29&u=f1ffa8eaccb8545222b7c642532161f11e74a03d&v=4')
+    no-repeat;
+
+  background-color: var(--gray);
+
+  position: relative;
+
+  &::after {
+    background-color: var(--status-online);
+    width: 10px;
+    height: 10px;
+
+    position: absolute;
+    bottom: -4px;
+    right: -4px;
+
+    border-radius: 12px;
+    border: 3px solid var(--quaternary);
+
+    content: '';
+  }
+`;
+
+export const UserData = styled.div`
+  margin-left: 8px;
+
+  display: flex;
+  flex-direction: column;
+
+  > strong {
+    color: var(--white);
+    font-size: 13px;
+    display: block;
+  }
+
+  > span {
+    color: var(--gray);
+    font-size: 13px;
+  }
+`;
+
+export const Icons = styled.div`
+  display: flex;
+  align-items: center;
+
+  > svg:not(:first-child) {
+    margin-left: 7px;
+  }
+`;
+
+export const MicIcon = styled(Mic)`
+  width: 20px;
+  height: 20px;
+
+  color: var(--white);
+  opacity: 0.7;
+  cursor: pointer;
+
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const HeadphoneIcon = styled(Headset)`
+  width: 20px;
+  height: 20px;
+
+  color: var(--white);
+  opacity: 0.7;
+  cursor: pointer;
+
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const SettingsIcon = styled(Settings)`
+  width: 20px;
+  height: 20px;
+
+  color: var(--white);
+  opacity: 0.7;
+  cursor: pointer;
+
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
