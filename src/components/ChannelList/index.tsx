a3c41891da36d37691fc0684373e771488e3cccb
Elton Lazzarin <53025782+eltonlazzarin@users.noreply.github.com>
2020-06-21 13:59:48 -0300

    main user list

--- src/components/ChannelList/index.tsx
+++ src/components/ChannelList/index.tsx
@@ -1,23 +1,87 @@
 import React from 'react';
 
-import ChannelButton from '../ChannelButton';
+import ServerButton from '../ServerButton';
 
-import { Container, Category, AddCategoryIcon } from './styles';
+import ReactTooltip from 'react-tooltip';
 
-const ChannelList: React.FC = () => {
+import { Container, Separator } from './styles';
+
+const ServerList: React.FC = () => {
   return (
     <Container>
-      <Category>
-        <span>Canais de texto</span>
-        <AddCategoryIcon />
-      </Category>
+      <a href="http://localhost:3000" data-for="home" data-tip="Home">
+        <ServerButton isHome />
+      </a>
+      <ReactTooltip
+        id="home"
+        place="right"
+        type="dark"
+        effect="solid"
+        offset={{ top: 4, left: -12 }}
+        className="custom"
+      />
 
-      <ChannelButton channelName="chat-livre" selected />
-      <ChannelButton channelName="lolzinho" />
-      <ChannelButton channelName="csgo" />
-      <ChannelButton channelName="valorant" />
+      <Separator />
+
+      <a href="http://localhost:3000" data-for="server" data-tip="Rocketseat">
+        <ServerButton hasNotifications server mentions={9} />
+      </a>
+      <ReactTooltip
+        id="server"
+        place="right"
+        type="dark"
+        effect="solid"
+        offset={{ top: 4, left: -12 }}
+        className="custom"
+      />
+
+      <a href="http://localhost:3000" data-for="add" data-tip="Add a Server">
+        <ServerButton add />
+      </a>
+      <ReactTooltip
+        id="add"
+        place="right"
+        type="dark"
+        effect="solid"
+        offset={{ top: 4, left: -12 }}
+        className="custom"
+      />
+
+      <a
+        href="http://localhost:3000"
+        data-for="explore"
+        data-tip="Explore Public Servers"
+      >
+        <ServerButton explore />
+      </a>
+      <ReactTooltip
+        id="explore"
+        place="right"
+        type="dark"
+        effect="solid"
+        offset={{ top: 4, left: -12 }}
+        className="custom"
+      />
+
+      <Separator />
+
+      <a
+        href="http://localhost:3000"
+        data-for="download"
+        data-tip="Download Apps"
+      >
+        <ServerButton download />
+      </a>
+      <ReactTooltip
+        id="download"
+        place="right"
+        type="dark"
+        effect="solid"
+        offset={{ top: 4, left: -12 }}
+        className="custom"
+      />
     </Container>
   );
 };
 
-export default ChannelList;
+export default ServerList;
